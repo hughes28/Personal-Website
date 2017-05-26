4f66e95ca7f7ae532b79fcbd90cd564c86a9ea0c
John Hughes <jfh4193@g.rit.edu>
2017-05-25 23:22:42 -0400

    Updated WorkExperience and Education

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -1,18 +1,30 @@
 class App extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {showHome: true};
+		this.state = {selectedNavBarItem: "Home"};
 		this.navbarListItemClicked = this.navbarListItemClicked.bind(this);
 	}
-	navbarListItemClicked(){
-		this.setState({showHome: !this.state.showHome}) 
+	navbarListItemClicked(item){
+		this.setState({selectedNavBarItem: item}) 
 	}
 	render(){
 		let componentToShow;
-		if(this.state.showHome === true){
-			componentToShow = <Home />;
-		} else {
-			componentToShow = <About />;
+		switch(this.state.selectedNavBarItem){
+			case "Home":
+				componentToShow = <Home />;
+				break;
+			case "About":
+				componentToShow = <About />;
+				break;
+			case "Education":
+				componentToShow = <Education />;
+				break;
+			case "WorkExperience":
+				componentToShow = <WorkExperience />;
+				break;
+			default:
+				componentToShow = <Home />;
+				break;
 		}
 		return (
 		<div>
@@ -30,10 +42,10 @@ class NavBar extends React.Component{
 		return (
 			<div>
 				<ul>
-					<li>Home</li>
-					<li onClick={this.props.navbarListItemClicked}>About</li>
-					<li>Education</li>
-					<li>Work Experience</li>
+					<li onClick={() => this.props.navbarListItemClicked("Home")}>Home</li>
+					<li onClick={() => this.props.navbarListItemClicked("About")}>About</li>
+					<li onClick={() => this.props.navbarListItemClicked("Education")}>Education</li>
+					<li onClick={() => this.props.navbarListItemClicked("WorkExperience")}>Work Experience</li>
 				</ul>
 			</div>
 
@@ -58,6 +70,48 @@ class About extends React.Component{
 			<div>
 				<h1>About Me</h1>
 				<h3>I am a recent graduate.</h3>
+			</div>
+		);
+	}
+}
+
+class Education extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>Education</h1>
+				<ul>
+					<li><h3>M.Sc, Microelectronic Engineering, RIT</h3></li>
+					<li><h3>B.Sc, Chemical Engineering, RIT</h3></li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+class WorkExperience extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>Work Experience</h1>
+				<ul>
+					<li>
+						<h2>Northrop Grumman Corporation</h2>
+						<h3>Technical Engineer Intern</h3>
+					</li>
+					<li>
+						<h2>Harris Space and Intelligence Systems</h2>
+						<h3>Manufacturing Engineer Intern</h3>
+					</li>
+					<li>
+						<h2>Volvo Group</h2>
+						<h3>Manufacturing Engineer Co-op</h3>
+					</li>
+					<li>
+						<h2>United Refining Company</h2>
+						<h3>Process Engineer Co-op</h3>
+					</li>
+				</ul>
 			</div>
 		);
 	}
