a4a8b7ca6491aea929077c3e2472abc5db6a8554
John Hughes <jfh4193@g.rit.edu>
2017-06-03 23:16:17 -0400

    fix styles (auto scale)

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -66,7 +66,7 @@ class Home extends React.Component{
 			<div className="home-component">
 				<h1 className="home-title-name">John F. Hughes</h1>
 				<h2 className="home-subtitle-about">Mad Chemist | Coffee Aficionado | Difference-Maker</h2>
-				<ul className="horizontal-ul home-social-btns">
+				<ul className="home-social-btns">
                		<li><a href="https://github.com/hughes28"><i className="fa fa-github"></i></a></li>
               		<li><a href="http://www.linkedin.com/in/johnfhughes"><i className="fa fa-linkedin"></i></a></li>
               		<li><a href="./Files/HughesResume.pdf"><i className="fa fa-id-badge"></i></a></li>
