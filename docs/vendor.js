8f99d99eca7009d98ba348f1df0a0cc18955a16d
John Hughes <jfh4193@g.rit.edu>
2017-06-18 00:01:43 -0400

    new styles in education section

--- docs/vendor.js
+++ docs/vendor.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 199);
+/******/ 	return __webpack_require__(__webpack_require__.s = 200);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -2278,7 +2278,8 @@ eval("/* WEBPACK VAR INJECTION */(function(process) {/**\n * Copyright 2013-pres
 /* 196 */,
 /* 197 */,
 /* 198 */,
-/* 199 */
+/* 199 */,
+/* 200 */
 /* no static exports found */
 /* all exports used */
 /*!*****************************!*\
