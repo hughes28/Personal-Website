bcbccabb1f190d6aa89d92ef7933a5fea5e61547
John Hughes <jfh4193@g.rit.edu>
2017-08-05 16:04:51 -0400

    made further JS development in LongTweetSplit component

--- client/Components/LongTweetSplit.jsx
+++ client/Components/LongTweetSplit.jsx
@@ -4,13 +4,73 @@ import ReactToggle from 'react-toggle-button';
 export default class LongTweetSplit extends React.Component{
 	constructor(props){
 		super(props);
-		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.getMessage = this.getMessage.bind(this);
+		this.state = {message: ""};
 	}
 
-	
-	render(){
+	handleChange(event) {
+		this.setState({message: event.target.value});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.getMessage();
+	}
+
+	getMessage() {
+		let message = this.state.message;
+		const maxTweetLength = 140;
+		const addedLength = 6;
+		const result = [];
+		let newMessage = "";
+		let numberOfTweets = 0;
+		let canContinue = true;
+
+		if (message.length > maxTweetLength) {
+			while (message.length > maxTweetLength) {
+				for (let i = maxTweetLength - 1 - addedLength; i >= 0; i--) {
+					if (message[i] === ' ') {
+						result.push(message.substring(0,i));
+						message = message.slice(i);
+						i = maxTweetLength;
+						numberOfTweets++;	
+						canContinue = false;
+						break;
+					}
+				}
+				if (canContinue) {
+						result.push(message.substring(0,maxTweetLength-1));
+						message = message.slice(maxTweetLength-1);
+						numberOfTweets++;
+				}
+			}
+
+			result.push(message);
+			numberOfTweets++;
+
+			for (let j = 0; j<result.length; j++) {
+				let tweetPage = j+1;
+				result[j] += " (" + tweetPage + "/" + numberOfTweets + ")";
+			}
+
+			for (let s in result) {
+				newMessage += result[s];
+			}
+
+			this.setState({message: newMessage});
+		}
+
+
+	} 
+
+	render() {
+
+		content = this.state.message;
+
 		return (
-			<div>
+			<div className = "widget-component">
 				<form onSubmit={this.handleSubmit}>
         			<label>
           			Enter tweet:
@@ -18,6 +78,7 @@ export default class LongTweetSplit extends React.Component{
         			</label>
         			<input type="submit" value="Submit" />
       			</form>
+      			{content}
 			</div>
 			
 		);
